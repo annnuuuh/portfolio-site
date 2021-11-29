e155a41dc932ac0d407ef9455194ba3b6912ae84
Anna Johnson <ae.johnson2931@gmail.com>
2021-11-29 15:43:39 -0700

    Create header with styling

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Nav from './Nav';
+import './App.css';
+
+const App = () => {
+  return (
+    <main className="App">
+      <header>
+        <h1>Desert Form Design</h1>
+        <Nav />
+      </header>
+    </main>
+  )
+}
+
+export default App;
